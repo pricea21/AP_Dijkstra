2c1b1dd46ae83d7956964f33658e11d8f6e1c98b
pricea21 <pricea26@my.erau.edu>
2024-02-21 12:48:17 -0700

    assets

---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    public Node[] ConnectsTo;
+
+    private void OnDrawGizmos()
+    {
+        foreach (Node n in ConnectsTo)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawRay(transform.position, (n.transform.position - transform.position).normalized * 2);
+        }
+    }
+
+}
